7c5ff173b64c5d9ed4f2f98cc78008a4e35b3aba
victor <victorkibichy>
2024-05-11 17:36:36 +0300

    Initial Commit

---
+++ Equifarm/EquifarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquifarmApp.swift
+//  Equifarm
+//
+//  Created by  Bouncy Baby on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EquifarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
